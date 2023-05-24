a8acf6f027c4fbae4dc706f86bb21d6367dabf01
SunRiz3r <130283689+SunRiz3r@users.noreply.github.com>
2023-05-24 13:15:56 +0600

    Добавление уведомлении

--- account/profileworker.php
+++ account/profileworker.php
@@ -1,5 +1,8 @@
 <?php
-session_start()
+session_start();
+require_once '../vendor/connect.php';
+
+$applications = mysqli_query($connect, "SELECT `IIN` FROM `application`")
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,7 +18,15 @@ session_start()
 </head>
 <body>
     <div class="wrapper">
-        
+        <?php
+        if (mysqli_num_rows($applications) > 0){
+            while($row = mysqli_fetch_assoc($applications)){
+                echo '<br>Заявка от '.$row["IIN"];
+            }
+        }else{
+            echo "У вас нет уведомлении";
+        }
+        ?>
     </div>
 </body>
 </html>
