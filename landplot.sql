9b16f76c62760c1798b4a93bb4f263c7d9842da3
SunRiz3r <130283689+SunRiz3r@users.noreply.github.com>
2023-05-26 12:11:44 +0600

    Add files via upload

--- landplot.sql
+++ landplot.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 23, 2023 at 10:16 AM
+-- Generation Time: May 26, 2023 at 06:11 AM
 -- Server version: 5.7.24
 -- PHP Version: 8.0.1
 
@@ -24,16 +24,46 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `application`
+-- Table structure for table `accounts`
 --
 
-CREATE TABLE `application` (
+CREATE TABLE `accounts` (
   `id` int(11) NOT NULL,
-  `IIN` varchar(12) NOT NULL,
-  `area` int(11) NOT NULL,
-  `landAddress` varchar(255) NOT NULL,
-  `specialPurpose` varchar(255) NOT NULL,
-  `partitionPurpose` varchar(255) NOT NULL
+  `name` varchar(255) NOT NULL,
+  `iin` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `certificateId` varchar(255) NOT NULL,
+  `region` varchar(255) NOT NULL,
+  `disctrict` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `accounts`
+--
+
+INSERT INTO `accounts` (`id`, `name`, `iin`, `pass`, `status`, `certificateId`, `region`, `disctrict`, `city`, `address`) VALUES
+(1, 'Сеитбеков Айбек Қалдарбекұлы', '020407501146', '1231', 'head', '1234', 'г.Астана', 'Алматинский', 'г.Астана', 'где-то там'),
+(2, 'Утаров Алибек Жанаталапович', '030720000010', '1234', 'user', '1234', 'г.Астана', 'Есил', 'г.Астана', 'тоже где-то там'),
+(3, 'Хасенов Алтай Елтайұлы', '040209550012', '1235', 'worker', '1234', 'г.Астана', 'Алматинский', 'г.Астана', 'вообще где-то там');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `landdivide`
+--
+
+CREATE TABLE `landdivide` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `iin` varchar(255) NOT NULL,
+  `certificateId` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `application` varbinary(3000) NOT NULL,
+  `scheme` varbinary(3000) NOT NULL,
+  `copy` varbinary(3000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -41,9 +71,15 @@ CREATE TABLE `application` (
 --
 
 --
--- Indexes for table `application`
+-- Indexes for table `accounts`
 --
-ALTER TABLE `application`
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `landdivide`
+--
+ALTER TABLE `landdivide`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -51,10 +87,16 @@ ALTER TABLE `application`
 --
 
 --
--- AUTO_INCREMENT for table `application`
+-- AUTO_INCREMENT for table `accounts`
 --
-ALTER TABLE `application`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `accounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `landdivide`
+--
+ALTER TABLE `landdivide`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
