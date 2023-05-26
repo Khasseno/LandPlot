71d5d313c3805fac6eb888e544271ba3d747f941
Алтай Хасенов <107077147+Khasseno@users.noreply.github.com>
2023-05-26 11:58:39 +0600

    Обновил страницу пользователя

--- script/profile.js
+++ script/profile.js
@@ -1,24 +1,13 @@
 const button = document.getElementById("button");
-const buttonRect = button.getBoundingClientRect();
-const chevron = document.getElementById("chevron");
-const chevronRect = chevron.getBoundingClientRect();
-const menuRight = buttonRect.right - chevronRect.right;
-const menuTop = chevronRect.top - buttonRect.top;
 
-const menu = document.getElementById("menu");
-menu.style.top = menuTop + "px";
-menu.style.right = menuRight + "px";
+function toggleDropDown() {
+    var menu = document.getElementById("menu");
+    var button = document.getElementById("menu__button")
+    if (button.classList.contains("open")) {
+        menu.style.opacity = '0%';
+    } else {
+        menu.style.opacity = '100%';
+    }
 
-const toggleDropdown = () => {
-  const dropdown = document.getElementById("dropdown");
-
-  if (dropdown.classList.contains("open")) {
-    menu.style.top = menuTop + "px";
-    menu.style.right = menuRight + "px";
-  } else {
-    menu.style.top = (parseInt(button.clientHeight) + parseInt(10)) + "px";
-    menu.style.right = 0;
-  }
-
-  dropdown.classList.toggle("open");
-};
+    button.classList.toggle("open")
+}
