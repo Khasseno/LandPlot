368fbc7fdfa2911a7be3d9c17e4439ade15aebf7
Алтай Хасенов <107077147+Khasseno@users.noreply.github.com>
2023-05-26 11:57:04 +0600

    Обновил страницу пользователя

--- account/profileuser.php
+++ account/profileuser.php
@@ -1,10 +1,11 @@
 <?php
-session_start();
-require_once '../vendor/connect.php';
+    session_start();
+    require_once '../vendor/connect.php';
 
-$Login = $_SESSION['name'];
-$applications = mysqli_query($connect, "SELECT `id` FROM `application` WHERE `Login`='$Login'")
+    $Login = $_SESSION['name'];
+    $applications = mysqli_query($connect, "SELECT `id` FROM `application` WHERE `Login`='$Login'")
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,42 +13,40 @@ $applications = mysqli_query($connect, "SELECT `id` FROM `application` WHERE `Lo
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/profile.css">
+    <link rel="stylesheet" href="../css/profileuser.css">
     <title>
         Профиль
     </title>
 </head>
+
 <body>
-    <div class="dropdown" id="dropdown">
-      <button id="button" onclick="toggleDropdown()">
-        <?php
-          echo $_SESSION['name'];
-        ?>
-        <span id="chevron" class="chevron"></span>
-      </button>
-      <div id="menu" class="menu">
-        <button>
-          Услуги
-        </button>
-        <button>
-          Выход
-        </button>
-      </div>
+    <header>
+        <ul>
+            <li class="fullname"> Хасенов Алтай Елтайулы
+                <a onclick="toggleDropDown()">
+                    <svg version="1.1" width="20px" height="20px" id="menu__button">
+                        <polygon fill="#b5cfc2" points="0,20 10,0 20,20" />
+                    </svg>
+                </a>
+            </li>
+        </ul>
+    </header>
+    <nav>
+        <ul class="menu" id="menu" style="display: inline;">
+            <a>
+                <li> Услуги </li>
+            </a>
+            <a>
+                <li> Выход </li>
+            </a>
+        </ul>
+    </nav>
+
+    <div class="wrapper">
+        <p class="applications"> Заявки </p>
     </div>
-      <div class="notification-container">
-        <h3 class="notification-title">Ваши заявления</h3>
-        <div class="notification-button full-width">
-        <?php
-        if (mysqli_num_rows($applications) > 0){
-            while($row = mysqli_fetch_assoc($applications)){
-                echo '<br>Ваша заявка под №'.$row["id"].' ещё в рассмотрении';
-            }
-        }else{
-            echo "Заявления отсутствуют";
-        }
-        ?>
-        </div>
-      </div>
-    </body>
+
     <script type="text/javascript" src="../script/profile.js"></script>
+</body>
+
 </html>
