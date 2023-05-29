fdd76a1a73303a82353d89edb210338d0d3877e7
SunRiz3r <130283689+SunRiz3r@users.noreply.github.com>
2023-05-29 13:23:26 +0600

    Update profileuser.php

--- account/profileuser.php
+++ account/profileuser.php
@@ -1,9 +1,14 @@
 <?php
-    session_start();
-    require_once '../vendor/connect.php';
+session_start();
+require_once '../vendor/connect.php';
 
-    $Login = $_SESSION['name'];
-    $applications = mysqli_query($connect, "SELECT * FROM `application` WHERE `Login`='$Login'")
+if($_SESSION['status'] != "user") header("Location: authorization.php");
+
+$iin = $_SESSION['iin'];
+$applications = mysqli_query($connect, "SELECT * FROM `landdivide` ORDER BY `time` DESC");
+$name = mysqli_fetch_assoc(mysqli_query($connect, "SELECT `name` FROM `accounts` WHERE `iin`='$iin'"))['name'];
+
+$landdivide = mysqli_query($connect, "SELECT * FROM `landdivide`");
 ?>
 
 <!DOCTYPE html>
@@ -15,7 +20,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/profileuser.css">
     <title>
-        Профиль
+    <?php echo $name; ?>
     </title>
 </head>
 
@@ -26,7 +31,7 @@
     </div>
 
     <div class="header">
-        <span class="fullname">Ким Татьяна Алексеевна</span>
+        <span class="fullname"><?php echo $name; ?></span>
         <div class="navigator">
             <button id="menu-btn" class="menu-btn" onClick="toggleDropDown()">
                 <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" style="fill:#b5cfc2;">
@@ -35,7 +40,7 @@
             </button>
             <ul id="menu" class="menu">
                 <li><a href="#">УСЛУГИ</a></li>
-                <li><a href="#">ВЫХОД</a></li>
+                <li><a href="authorization.php">ВЫХОД</a></li>
             </ul>
         </div>
     </div>
@@ -43,34 +48,39 @@
     <div class="wrapper">
         <p class="application-title">ЗАЯВКИ</p>
         <ul class="application-list">
-            <li class="application">
-                <div class="application-name">Заявление на определение делимости или неделимости земельного участкаs</div>
-                <div class="application-status">принято</div>
-                <div class="application-save">скачать</div>
-            </li>
-            <li class="application">
-                <div class="application-name">Заявление на определение делимости или неделимости земельного участкаs</div>
-                <div class="application-status deny">отказано</div>
-                <div class="application-save">скачать</div>
-            </li>
-
-            <li class="application">
-                <div class="application-name">Заявление на определение делимости или неделимости земельного участкаs</div>
-                <div class="application-status success">выдано</div>
-                <div class="application-save">скачать</div>
-            </li>
-
-            <li class="application">
-                <div class="application-name">Заявление на определение делимости или неделимости земельного участкаs</div>
-                <div class="application-status">принято</div>
-                <div class="application-save">скачать</div>
-            </li>
-
-            <li class="application">
-                <div class="application-name">Заявление на определение делимости или неделимости земельного участкаs</div>
-                <div class="application-status">принято</div>
-                <div class="application-save">скачать</div>
-            </li>
+            <?php
+            if(mysqli_num_rows($applications) > 0){
+                for($i = 0; $i < mysqli_num_rows($applications); $i++){
+                    switch(mysqli_fetch_assoc($applications)['status']){
+                    case 'sent':
+                        echo '<li class="application">
+                        <div class="application-name">Заявление на определение делимости или неделимости земельного участкаs</div>
+                        <div class="application-status '.mysqli_fetch_assoc($applications)['status'].'">отправлено</div>
+                        </li>';
+                        break;
+                    case 'accepted':
+                        echo '<li class="application">
+                        <div class="application-name">Заявление на определение делимости или неделимости земельного участкаs</div>
+                        <div class="application-status '.mysqli_fetch_assoc($applications)['status'].'">принято</div>
+                        </li>';
+                        break;
+                    case 'success':
+                        echo '<li class="application">
+                        <div class="application-name">Заявление на определение делимости или неделимости земельного участкаs</div>
+                        <div class="application-status '.mysqli_fetch_assoc($applications)['status'].'">выдано</div>
+                        <div class="application-save">скачать</div>
+                        </li>';
+                        break;
+                    case 'deny':
+                        echo '<li class="application">
+                        <div class="application-name">Заявление на определение делимости или неделимости земельного участкаs</div>
+                        <div class="application-status '.mysqli_fetch_assoc($applications)['status'].'">отказано</div>
+                        <div class="application-save">скачать</div>
+                        </li>';
+                        break;
+                    }
+                }
+            }?>
         </ul>
     </div>
 
