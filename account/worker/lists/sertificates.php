5fd08b76372ae8df26e1a7411b7ab6fa9ed80f82
pialushnik <114019198+pialushnik@users.noreply.github.com>
2023-05-29 12:23:28 +0600

    Добавила страницы списков заявок

---
+++ account/worker/lists/sertificates.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../../css/sertificates.css">
+</head>
+<body>
+        <div class="line1"></div>    
+        <div class="line2"></div>
+        <a href="#"><div class="triangle"></div></a>
+        <div class="text">Выдача жилищных сертификатов</div>
+        <ul class="list">
+            <li class="application"> 
+                <div class="fio"><h4>Фамилия Имя Отчество</h4></div>
+                <div class="number"><h4>номер заявки</h4></div>
+                <a href="#"><div class="view"><h4>просмотр</h4></div></a>
+            </li>
+
+            <li class="application"> 
+                <div class="fio"><h4>Фамилия Имя Отчество</h4></div>
+                <div class="number"><h4>номер заявки</h4></div>
+                <a href="#"><div class="view"><h4>просмотр</h4></div></a>
+            </li>
+
+            <li class="application"> 
+                <div class="fio"><h4>Фамилия Имя Отчество</h4></div>
+                <div class="number"><h4>номер заявки</h4></div>
+                <a href="#"><div class="view"><h4>просмотр</h4></div></a>
+            </li>
+
+            <li class="application"> 
+                <div class="fio"><h4>Фамилия Имя Отчество</h4></div>
+                <div class="number"><h4>номер заявки</h4></div>
+                <a href="#"><div class="view"><h4>просмотр</h4></div></a>
+            </li>
+
+            <li class="application"> 
+                <div class="fio"><h4>Фамилия Имя Отчество</h4></div>
+                <div class="number"><h4>номер заявки</h4></div>
+                <a href="#"><div class="view"><h4>просмотр</h4></div></a>
+            </li>
+        </ul>        
+</body>
+</html>
