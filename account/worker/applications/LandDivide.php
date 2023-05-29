e94d487693cc42e58f4bd9b43215f08c8f4e3a4a
pialushnik <114019198+pialushnik@users.noreply.github.com>
2023-05-29 16:55:29 +0600

    Add files via upload

---
+++ account/worker/applications/LandDivide.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../../css/LandDivide.css">
+</head>
+<body>
+        <div class="line1"></div>    
+        <div class="line2"></div>
+        <a href="#">
+            <div class="triangle">
+        </div></a>
+        <div class="text"> Просмотр заявки "Определение делимости и неделимости земельного участка"</div>
+        <div class="personInfo">
+            <h1>ЗАЯВКА № "номер заявки"</h1>
+            <p>Сведения о заявителе:</p>
+            <div class="person">
+                <div class="person-name">Фамилия Имя Отчество</div>
+                <div class="person-iin">ИИН</div>
+            </div>
+            <div class="home-address">Домашний адрес</div>
+        </div>
+        <div class="documents">
+            <p>Предоставляемые документы:</p>
+            <div class="person-statement">
+                <div class="statement">заявление на определение делимости и неделимости земельного участка</div>
+                <a href="#" class="pdf-statement">прикрепленный документ</a>
+            </div>
+
+            <div class="person-statement">
+                <div class="statement">схема раздела земельного участка, с указанием размеров разделяемых частей земельного участка и существующих строений</div>
+                <a href="#" class="pdf-statement">прикрепленный документ</a>
+            </div>
+
+            <div class="person-statement">
+                <div class="statement">копия идентификационного документа на земельный участок</div>
+                <a href="#" class="pdf-statement">прикрепленный документ</a>
+            </div>
+        </div>
+        <div class="buttons">
+            <a href="#" class="yes">отправить на согласование</a>
+            <a href="#" class="no">отклонить</a>
+        </div>
+</body>
