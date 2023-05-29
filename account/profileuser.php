41469ad9227ccb74e64a4c68e1e6d120ef3633a9
Алтай Хасенов <107077147+Khasseno@users.noreply.github.com>
2023-05-29 11:44:24 +0600

    Идеальная обнова страницы пользователя

--- account/profileuser.php
+++ account/profileuser.php
@@ -3,7 +3,7 @@
     require_once '../vendor/connect.php';
 
     $Login = $_SESSION['name'];
-    $applications = mysqli_query($connect, "SELECT `id` FROM `application` WHERE `Login`='$Login'")
+    $applications = mysqli_query($connect, "SELECT * FROM `application` WHERE `Login`='$Login'")
 ?>
 
 <!DOCTYPE html>
@@ -20,33 +20,60 @@
 </head>
 
 <body>
-    <header>
-        <ul>
-            <li class="fullname"> Хасенов Алтай Елтайулы
-                <a onclick="toggleDropDown()">
-                    <svg version="1.1" width="20px" height="20px" id="menu__button">
-                        <polygon fill="#b5cfc2" points="0,20 10,0 20,20" />
-                    </svg>
-                </a>
-            </li>
-        </ul>
-    </header>
-    <nav>
-        <ul class="menu" id="menu" style="display: inline;">
-            <a>
-                <li> Услуги </li>
-            </a>
-            <a>
-                <li> Выход </li>
-            </a>
-        </ul>
-    </nav>
+    <div class="lines">
+        <div class="line"></div>
+        <div class="line2"></div>
+    </div>
+
+    <div class="header">
+        <span class="fullname">Ким Татьяна Алексеевна</span>
+        <div class="navigator">
+            <button id="menu-btn" class="menu-btn" onClick="toggleDropDown()">
+                <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" style="fill:#b5cfc2;">
+                    <polygon points="0,4 20,4 10,20"></polygon>
+                </svg>
+            </button>
+            <ul id="menu" class="menu">
+                <li><a href="#">УСЛУГИ</a></li>
+                <li><a href="#">ВЫХОД</a></li>
+            </ul>
+        </div>
+    </div>
 
     <div class="wrapper">
-        <p class="applications"> Заявки </p>
+        <p class="application-title">ЗАЯВКИ</p>
+        <ul class="application-list">
+            <li class="application">
+                <div class="application-name">Заявление на определение делимости или неделимости земельного участкаs</div>
+                <div class="application-status">принято</div>
+                <div class="application-save">скачать</div>
+            </li>
+            <li class="application">
+                <div class="application-name">Заявление на определение делимости или неделимости земельного участкаs</div>
+                <div class="application-status deny">отказано</div>
+                <div class="application-save">скачать</div>
+            </li>
+
+            <li class="application">
+                <div class="application-name">Заявление на определение делимости или неделимости земельного участкаs</div>
+                <div class="application-status success">выдано</div>
+                <div class="application-save">скачать</div>
+            </li>
+
+            <li class="application">
+                <div class="application-name">Заявление на определение делимости или неделимости земельного участкаs</div>
+                <div class="application-status">принято</div>
+                <div class="application-save">скачать</div>
+            </li>
+
+            <li class="application">
+                <div class="application-name">Заявление на определение делимости или неделимости земельного участкаs</div>
+                <div class="application-status">принято</div>
+                <div class="application-save">скачать</div>
+            </li>
+        </ul>
     </div>
 
     <script type="text/javascript" src="../script/profile.js"></script>
 </body>
-
 </html>
