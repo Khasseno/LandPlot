bd9eef3b70598d73ab248edf575bb0c583bec145
Алтай Хасенов <107077147+Khasseno@users.noreply.github.com>
2023-05-29 16:56:40 +0600

    Add files via upload

---
+++ account/worker/applications/certificate.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Просмотр</title>
+    <link rel="stylesheet" href="../../../css/certificate.css">
+</head>
+
+<body>
+    <div class="line1"></div>
+    <div class="line2"></div>
+    <a href="#">
+        <div class="triangle"></div>
+    </a>
+    <div class="text"> Просмотр заявки "Выдача жилищный сертификатов"</div>
+    <div class="personInfo">
+        <h1>ЗАЯВКА № "номер заявки"</h1>
+        <p>Сведения о заявителе:</p>
+        <div class="person">
+            <div class="person-name">ФИО</div>
+            <div class="person-iin">123456123456</div>
+        </div>
+
+        <div class="spouse">
+            <div class="spouse-name">ФИО</div>
+            <div class="spouse-iin">123456123456</div>
+        </div>
+
+        <ul class="children">
+            <li class="child">
+                <div class="child-name">ФИО</div>
+                <div class="child-iin">123456123456</div>
+            </li>
+
+            <li class="child">
+                <div class="child-name">ФИО</div>
+                <div class="child-iin">123456123456</div>
+            </li>
+
+            <li class="child">
+                <div class="child-name">ФИО</div>
+                <div class="child-iin">123456123456</div>
+            </li>
+
+            <li class="child">
+                <div class="child-name">ФИО</div>
+                <div class="child-iin">123456123456</div>
+            </li>
+
+            <li class="child">
+                <div class="child-name">ФИО</div>
+                <div class="child-iin">123456123456</div>
+            </li>
+
+            <li class="child">
+                <div class="child-name">ФИО</div>
+                <div class="child-iin">123456123456</div>
+            </li>
+
+            <li class="child">
+                <div class="child-name">ФИО</div>
+                <div class="child-iin">123456123456</div>
+            </li>
+
+            <li class="child">
+                <div class="child-name">ФИО</div>
+                <div class="child-iin">123456123456</div>
+            </li>
+
+            <li class="child">
+                <div class="child-name">ФИО</div>
+                <div class="child-iin">123456123456</div>
+            </li>
+        </ul>
+    </div>
+
+
+    <div class="documents">
+        <p>Предоставляемые документы:</p>
+        <div class="person-statement">
+            <div class="statement">заявление на предоставление жилищного сертификата</div>
+            <a href="#" class="pdf-statement">прикрепленный документ</a>
+        </div>
+
+        <div class="person-statement">
+            <div class="statement">схема раздела земельного участка, с указанием размеров разделяемых частей земельного
+                участка</div>
+            <a href="#" class="pdf-statement">прикрепленный документ</a>
+        </div>
+
+        <div class="person-statement">
+            <div class="statement">справка о доходах за последние шесть месяцев по трудовой и (или) предпринимательской
+                деятельности (на всех членов семьи)</div>
+            <a href="#" class="pdf-statement">прикрепленный документ</a>
+        </div>
+
+        <div class="person-statement">
+            <div class="statement">справка с места работы, за исключением социально-уязвимых слоев населения</div>
+            <a href="#" class="pdf-statement">прикрепленный документ</a>
+        </div>
+
+        <div class="person-statement">
+            <div class="statement">письмо БВУ об одобрении выдачи ипотечного займа при приобретении жилья заявителю
+            </div>
+            <a href="#" class="pdf-statement">прикрепленный документ</a>
+        </div>
+    </div>
+
+    <div class="buttons">
+        <a href="#" class="yes">отправить на согласование</a>
+        <a href="#" class="no">отклонить</a>
+    </div>
+</body>
+
+</html>
