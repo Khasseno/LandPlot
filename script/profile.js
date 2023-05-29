00b892f8b3a666f8564173a79b2f91ad059649aa
Алтай Хасенов <107077147+Khasseno@users.noreply.github.com>
2023-05-29 11:51:30 +0600

    Идеальная обнова страницы пользователя

--- script/profile.js
+++ script/profile.js
@@ -1,12 +1,13 @@
-const button = document.getElementById("button");
-
 function toggleDropDown() {
     var menu = document.getElementById("menu");
-    var button = document.getElementById("menu__button")
+    var button = document.getElementById("menu-btn");
+
     if (button.classList.contains("open")) {
         menu.style.opacity = '0%';
+        menu.style.visibility = "hidden";
     } else {
         menu.style.opacity = '100%';
+        menu.style.visibility = "visible";
     }
 
     button.classList.toggle("open")
