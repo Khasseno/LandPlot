21d2a73c620dcdebec1ab47a080e0e3e2ac5392c
pialushnik <114019198+pialushnik@users.noreply.github.com>
2023-05-30 17:26:51 +0600

    Add files via upload

---
+++ account/user/sertificateApplication.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../css/sertificateApplication.css">
+</head>
+<body>
+    <div class="line1"></div>    
+    <div class="line2"></div>
+    <a href="#">
+        <div class="triangle">
+    </div></a>
+    <div class="text">Выдача жилищных сертификатов</div>
+    <form class="info">
+        <div class="personalData">
+            <ul>
+                <p class="data">Личные данные</p>
+
+                <li class="iin">
+                    <input type="number" placeholder="Введите ИИН">
+                </li>
+
+                <li class="surname">
+                    <input type="text" placeholder="Введите фамилию">
+                </li>
+
+                <li class="name">
+                    <input type="text" placeholder="Введите имя">
+                </li>
+
+                <li class="patronymic">
+                    <input type="text" placeholder="Введите отчество">
+                </li>
+
+                <li class="id-number">
+                    <input type="number" placeholder="Введите № удостоверения личности">
+                </li>
+
+                <p class="family">Сведения о семье</p>
+
+                <li class="marriage-number">
+                    <input type="number" placeholder="Введите № свидетельства о заключени брака">
+                </li>
+
+                <li class="spouse">
+                    <input type="text" placeholder="Введите ФИО супруг(а)">
+                </li>
+
+                <li class="spouse-iin">
+                    <input type="number" placeholder="Введите ИИН супруг(а)">
+                </li>
+            </ul>
+        </div>
+        
+        <div class="address">
+            <ul>
+                <li class="amount-of-children">
+                    <input type="number" placeholder="Введите количество детей">
+                </li>
+            </ul>
+            
+            <div class="child">
+                <ul>
+                    <li>
+                        <input type="text" placeholder="ребенок 1">
+                    </li>
+
+                    <li>
+                        <input type="text" placeholder="ребенок 2">
+                    </li>
+
+                    <li>
+                        <input type="text" placeholder="ребенок 3">
+                    </li>
+
+                    <li>
+                        <input type="text" placeholder="ребенок 4">
+                    </li>
+                </ul>
+            </div>
+
+            <div class="child-iin">
+                <ul>
+                    <li>
+                        <input type="number" placeholder="ИИН ребенка 1">
+                    </li>
+
+                    <li>
+                        <input type="number" placeholder="ИИН ребенока 2">
+                    </li>
+
+                    <li>
+                        <input type="number" placeholder="ИИН ребенка 3">
+                    </li>
+
+                    <li>
+                        <input type="number" placeholder="ИИН ребенка 4">
+                    </li>
+                </ul>
+            </div>
+            <ul>
+                <p class="family">Данные по прописке</p>
+
+                <li class="region">
+                    <input type="text" placeholder="Введите область">
+                </li>
+
+                <li class="district">
+                    <input type="text" placeholder="Введите район">
+                </li>
+
+                <li class="city">
+                    <input type="text" placeholder="Введите город/село">
+                </li>
+
+                <li class="street">
+                    <input type="text" placeholder="Введите улицу, дом, квартиру">
+                </li>
+            </ul>
+
+        </div>
+    </form>
+    
+   <p class="next"><a href="#">ДАЛЕЕ</a></p>
+       
+</body>
+</html>
