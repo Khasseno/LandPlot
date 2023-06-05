ad712ce05a5fb0dbfc6e706fecb7b2ae8502b7c5
SunRiz3r <130283689+SunRiz3r@users.noreply.github.com>
2023-06-05 12:52:52 +0600

    Update certificateApplicationNext.php

--- account/user/sendApplications/certificateApplicationNext.php
+++ account/user/sendApplications/certificateApplicationNext.php
@@ -6,13 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <link rel="stylesheet" href="../../../css/pinDocuments.css"> 
-    <title>Выдача желищных сертификатов</title> 
+    <title>Выдача жилищных сертификатов</title> 
 </head> 
 <body>
      <div class="line1"></div>
      <div class="line2"></div>
      <a href="#"><div class="triangle"></div></a>
-     <div class="title">Выдача желищных сертификатов</div>
+     <div class="title">Выдача жилищных сертификатов</div>
 
      <div class="container-max">
         <div class="container">
