094fd6dd5de28cd8a4bfad7f8a77ee26a3fd6df0
Алтай Хасенов <107077147+Khasseno@users.noreply.github.com>
2023-06-08 05:12:02 +0600

    Update landDivideApplicationNext.php

--- account/user/sendApplications/landDivideApplicationNext.php
+++ account/user/sendApplications/landDivideApplicationNext.php
@@ -1,50 +1,48 @@
 <?php
-session_start();
+  session_start();
 
-if($_SESSION['status'] != "user") header("Location: authorization.php");
+  if($_SESSION['status'] != "user") header("Location: authorization.php");
 ?>
-<!DOCTYPE html> 
-<html lang="en"> 
- 
-<head> 
-    <meta charset="UTF-8"> 
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-    <link rel="stylesheet" href="../../../css/account/user/sendApplications/LandDivideApplicationNext.css"> 
-    <title>Определение делимости и неделимости земельного участка</title> 
-</head> 
-<body>
-     <div class="line1"></div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../../css/account/user/sendApplications/landDivideApplicationNext.css">
+</head>
+<body>  
+    <div class="line1"></div>
      <div class="line2"></div>
-     <a href="LandDivideApplication.php"><div class="triangle"></div></a>
+     <a href="landDivideApplication.php"><div class="triangle"></div></a>
      <div class="title">Определение делимости и неделимости земельного участка</div>
 
-     <div class="container-max">
+    <form action="../../../vendor/sendApplication/sendLandDivideApplication.php" method="POST" enctype="multipart/form-data" class="container-max">
+        
         <div class="container">
 
-          <div class="words">заявление на определение делимости и неделимости земельного участка</div>
+          <div class="words">Заявление на определение делимости и неделимости земельного участка</div>
           <label for="pin-doc">
-            <!-- <span class="pin-text">Прикрепить документ</span> -->
-            <input type="file" name="mainApplication" id="pin-doc">
+            <input type="file" name="landDivideApplication" id="pin-doc" accept="application/pdf" required>
           </label>
 
-          <div class="words">схема раздела земельного участка с указанием размеров разделяемых частей земельного участка и существующих строений</div>
+          <div class="words">Схема раздела земельного участка с указанием размеров разделяемых частей земельного участка и существующих строений</div>
           <label for="pin-doc">
-            <!-- <span class="pin-text">Прикрепить документ</span> -->
-            <input type="file" name="identifyDoc" id="pin-doc">
+            <input type="file" name="scheme" id="pin-doc" accept="application/pdf" required>
           </label>
           
-          <div class="words">копия идентификационного документа на земельной участок</div>
+          <div class="words">Копия идентификационного документа на земельный участок</div>
           <label for="pin-doc">
-            <!-- <span class="pin-text">Прикрепить документ</span> -->
-            <input type="file" name="incomeStatement" id="pin-doc">
+            <input type="file" name="idCopy" id="pin-doc" accept="application/pdf" required>
           </label>
+
         </div>
 
-        <button class="send">отправить заявку</button>
-      </div>
+        <button type="submit" class="send">отправить заявку</button>
+    </form>
 
-      <script>
+    <script>
         var inputs = Array.from(document.querySelectorAll('#pin-doc'));
         
         inputs.forEach(input => {
