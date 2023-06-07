2746eb1cd1a47a904dd25b03963c97e2f0fc8793
Алтай Хасенов <107077147+Khasseno@users.noreply.github.com>
2023-06-08 04:55:02 +0600

    Update certificateApplicationNext.php

--- account/user/sendApplications/certificateApplicationNext.php
+++ account/user/sendApplications/certificateApplicationNext.php
@@ -17,40 +17,40 @@ if($_SESSION['status'] != "user") header("Location: authorization.php");
      <div class="line1"></div>
      <div class="line2"></div>
      <a href="certificateApplication.php"><div class="triangle"></div></a>
-     <div class="title">Выдача жилищных сертификатов</div>
+     <div class="title">Выдача желищных сертификатов</div>
 
-     <div class="container-max">
+     <form action="../../../vendor/sendApplication/sendCertificateApplication.php" method="POST" enctype="multipart/form-data" class="container-max">
         <div class="container">
 
           <div class="words">заявление на предоставление жилищного сертификата</div>
           <label for="pin-doc">
-            <input type="file" name="mainApplication" id="pin-doc">
+            <input type="file" name="mainApplication" id="pin-doc" accept="application/pdf" required>
           </label>
 
           <div class="words">документ,удостоверяющий личность заявителя и членов семьи(супруг(а), несовершеннолетних детей)(для идентификации личности) </div>
           <label for="pin-doc">
-            <input type="file" name="identifyDoc" id="pin-doc">
+            <input type="file" name="identifyDoc" id="pin-doc" accept="application/pdf" required>
           </label>
           
           <div class="words">справка о доходах за последнии шесть месяцев по трудовой и (или) предпренимательской деятельности(на всех членов семьи)</div>
           <label for="pin-doc">
-            <input type="file" name="incomeStatement" id="pin-doc">
+            <input type="file" name="incomeStatement" id="pin-doc" accept="application/pdf" required>
           </label>
 
           <div class="words">справка с места работы,за исключением социально-уязвимых слоев население</div>
           <label for="pin-doc">
-            <input type="file" name="workStatement" id="pin-doc">
+            <input type="file" name="workStatement" id="pin-doc" accept="application/pdf" required>
           </label>
             
           <div class="words">письмо БВУ об одобрении выдачи ипотечного займа на приобретении жилья заявителю,содержащее сведения о сумме,размере о первоначального взноса и сумме ежемесячного платежа по ипотечному жилищному займу</div>
           <label for="pin-doc">
-            <input type="file" name="STBMassage" id="pin-doc">
+            <input type="file" name="STBMassage" id="pin-doc" accept="application/pdf" required>
           </label>
 
         </div>
 
-        <button class="send">отправить заявку</button>
-      </div>
+        <button type="submit" class="send">отправить заявку</button>
+      </form>
 
       <script>
         var inputs = Array.from(document.querySelectorAll('#pin-doc'));
