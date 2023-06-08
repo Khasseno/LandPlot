f8901b6387520909ac2ee59b7397364c3a5e9400
Алтай Хасенов <107077147+Khasseno@users.noreply.github.com>
2023-06-08 06:05:39 +0600

    Update certificates.php

--- account/head/lists/certificates.php
+++ account/head/lists/certificates.php
@@ -14,7 +14,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../../../css/account/worker/lists/applicationList.css">
+    <link rel="stylesheet" href="../../../css/account/head/lists/applicationList.css">
 </head>
 <body>
         <div class="line1"></div>    
